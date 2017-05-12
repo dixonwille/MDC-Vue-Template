f04a47389f725dc1885f86a36dfd587f65ae9c2f
Will Dixon <dixonwille@gmail.com>
2017-05-12 00:55:24 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <header class="mdc-toolbar mdc-toolbar--fixed">
+      <div class="mdc-toolbar__row">
+        <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
+          <span class="mdc-toolbar__title">MDC Vue JS Template</span>
+        </section>
+      </div>
+    </header>
+    <main class="mdc-toolbar-fixed-adjust">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script lang="babel">
+import { MDCToolbar } from '@material/toolbar';
+
+export default {
+  name: 'app',
+  mounted: function () {
+    const toolbar = new MDCToolbar(document.querySelector('.mdc-toolbar'));
+    toolbar.fixedAdjustElement = document.querySelector('.mdc-toolbar-fixed-adjust');
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+@import '@material/toolbar/mdc-toolbar';
+
+.mdc-toolbar__title{
+  background: no-repeat 0/32px url(./assets/logo.png);
+  padding: 2px 0 5px 50px;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+    <h1 class="mdc-typography--display1">{{ msg }}</h1>
+    <div class="mdc-layout-grid">
+      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+        <div class="mdc-card">
+          <section class="mdc-card__primary">
+            <h1 class="mdc-card__title mdc-card__title--large">Essential Links</h1>
+          </section>
+          <section class="mdc-card__supporting-text">
+            <ul class="mdc-list">
+              <li class="mdc-list-item"><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+              <li class="mdc-list-item"><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+              <li class="mdc-list-item"><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+              <li class="mdc-list-item"><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+              <li class="mdc-list-item"><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+            </ul>
+          </section>
+        </div>
+      </div>
+      <div class="mdc-list-grid__cell mdc-layout-grid__cell--span-6">
+        <div class="mdc-card">
+          <section class="mdc-card__primary">
+            <h1 class="mdc-card__title mdc-card__title--large">Ecosystem</h1>
+          </section>
+          <section class="mdc-card__supporting-text">
+            <ul class="mdc-list">
+              <li class="mdc-list-item"><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+              <li class="mdc-list-item"><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+              <li class="mdc-list-item"><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+              <li class="mdc-list-item"><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+            </ul>
+          </section>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@material/list/mdc-list';
+@import '@material/card/mdc-card';
+</style>
